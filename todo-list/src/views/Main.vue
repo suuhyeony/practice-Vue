012ad1de8eb5ceb4ceaaee9bd09835264a2f38f2
박수현3 [suuhyeony] <suuhyeony@nexon.co.kr>
2021-04-26 18:02:56 +0900

    [add] set pjt(todo-list)

---
+++ todo-list/src/views/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Todo List</h1>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data() {
+        return
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
